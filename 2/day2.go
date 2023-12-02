8023c248273799681c0f0a11784710ab6e038f3f
antooro <antoorodev@gmail.com>
2023-12-02 15:17:16 +0100

    Day 2 done in GoLang

--- 2/day2.go
+++ 2/day2.go
@@ -4,11 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode"
+	_ "reflect"
 	"strconv"
+	_ "strconv"
 	"strings"
 )
 
+
+func is_valid(iterations []string) bool{
+	for _ , element := range iterations { 
+        valores := strings.Split(element, ",")
+		limits := map[string]int{
+			"red": 12,
+			"green":  13,
+			"blue": 14,
+		}
+		for _, element := range valores { 
+			valores := strings.Split(strings.TrimSpace(element), " ")
+			color_name := valores[1]
+			color_value, _ := strconv.Atoi(valores[0])
+			if color_value > limits[color_name] {
+				return false
+			}
+		}
+	}
+	return true
+}
 func part_one() {
 	readFile, err := os.Open("input")
   
@@ -18,20 +39,70 @@ func part_one() {
     fileScanner := bufio.NewScanner(readFile)
  
     fileScanner.Split(bufio.ScanLines)
-	resultado := 0
+	sum := 0
     for fileScanner.Scan() {
-		cleared := ""
-		for _ , char := range fileScanner.Text() {
-			if unicode.IsDigit(char) {
-				cleared += string(char)
-			}
+		data := strings.Split(fileScanner.Text(), ":")
+		game_id := strings.Split(data[0], " ")
+		gid  := game_id[len(game_id)-1]
+		num_game_id, _ := strconv.Atoi(gid)
+		iterations := strings.Split(data[1], ";")
+
+		if is_valid(iterations){
+			sum += num_game_id
 		}
-		partialres := string(string(cleared[0]) + string(cleared[len(cleared)-1:]))
-		num, _ := strconv.Atoi(partialres)
-		resultado += num
 
     }
-	fmt.Printf(strconv.Itoa(resultado))
-
+	fmt.Printf("%v",sum)
     readFile.Close()
+}
+
+func part_two() {
+	readFile, err := os.Open("input")
+  
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+ 
+    fileScanner.Split(bufio.ScanLines)
+	sum := 0
+    for fileScanner.Scan() {
+		data := strings.Split(fileScanner.Text(), ":")
+		iterations := strings.Split(data[1], ";")
+		limits := map[string]int{
+			"red": 0,
+			"green":  0,
+			"blue": 0,
+		}
+		for _ , element := range iterations { 
+			valores := strings.Split(element, ",")
+			
+			for _, element := range valores { 
+				valores := strings.Split(strings.TrimSpace(element), " ")
+				color_name := valores[1]
+				color_value, _ := strconv.Atoi(valores[0])
+				if color_value > limits[color_name] {
+					limits[color_name] = color_value
+				}
+			}
+			
+		}
+		prod := 1
+		for _ , v := range limits{
+			prod *= v
+		}
+		sum += prod
+		
+
+		}
+		
+
+    
+	fmt.Printf("%v",sum)
+    readFile.Close()
+}
+
+func main(){
+	// part_one()
+	part_two()
 }
